5aef0360430e76e4febfeabf7db6cf191230b431
Mayank Sethi <mayank.sethi1@watchhabitat.com>
2019-12-17 19:32:00 +0530

    creating task list for Search Module

--- src/js/index.js
+++ src/js/index.js
@@ -10,5 +10,29 @@ import Search from "./models/Search"
 
 const state = {};
 
+const controlSearch = async () => {
+    
+    // 1) Get query from view
+    const query = `pizza`; //TODO
+
+    if(query){
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render the results on UI
+    }
+
+}
+
+document.querySelector(".search").addEventListener('submit', e=>{
+    e.preventDefault();
+    controlSearch();
+});
+
 const search = new Search(`pizza`);
 search.getResults();
