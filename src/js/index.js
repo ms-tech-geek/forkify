2ad881fa66e97cadc6651957a4d2cdcd70523614
Mayank Sethi <mayank.sethi1@watchhabitat.com>
2019-12-17 19:25:55 +0530

    creating task list

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,14 @@
 import Search from "./models/Search"
 
+
+/* Global State of App
+* - Search Object
+* - Current Recipe Object
+* - Shopping List Object
+* - Like Recipes
+*/
+
+const state = {};
+
 const search = new Search(`pizza`);
-console.log(search);
 search.getResults();
