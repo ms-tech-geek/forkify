0c365754575931612a6b3bb8c87d02c73366a42f
Mayank Sethi <mayank.sethi1@watchhabitat.com>
2019-12-18 07:49:49 +0530

    fetched searchInput and displaying result in console

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from "./models/Search"
+import * as searchView from "./views/searchView"
+import { elements } from "./views/base"
 
 
 /* Global State of App
@@ -13,7 +15,7 @@ const state = {};
 const controlSearch = async () => {
     
     // 1) Get query from view
-    const query = `pizza`; //TODO
+    const query = searchView.getInput();
 
     if(query){
         // 2) New search object and add to state
@@ -30,7 +32,7 @@ const controlSearch = async () => {
 
 }
 
-document.querySelector(".search").addEventListener('submit', e=>{
+elements.searchForm.addEventListener("submit", e => {
     e.preventDefault();
     controlSearch();
-});
+})
